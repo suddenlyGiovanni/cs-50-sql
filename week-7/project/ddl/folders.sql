67e88391f254a3e1f942cf17b2262f54e1978970
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-16 14:34:54 +0200

    Change SQL dialect and update tables and triggers
    Switched folders.sql dialect from SQLite to PostgreSQL. Updated table definitions, comments, and indices to align with PostgreSQL standards. Added triggers and functions for maintaining unique folder names and timestamps.

--- week-7/project/ddl/folders.sql
+++ week-7/project/ddl/folders.sql
@@ -1,48 +1,76 @@
 DROP TABLE IF EXISTS folders;
--- Folders represent the hierarchical folders structure.
--- The parent-child relationship is defined by a self-referencing foreign key for the subfolders
+DROP INDEX IF EXISTS folders_resource_id_index;
+DROP INDEX IF EXISTS folders_parent_folder_id_index;
+DROP TRIGGER IF EXISTS folders_name_unique_within_parent_trigger ON folders;
+DROP TRIGGER IF EXISTS folders_updated_at_trigger ON folders;
+
+
 CREATE TABLE IF NOT EXISTS folders (
-    id               INTEGER PRIMARY KEY AUTOINCREMENT,
-    name             TEXT     NOT NULL,                           -- Folder name
-    parent_folder_id INTEGER           DEFAULT NULL,              -- Reference to the parent folder can be NULL if it's a root folder
-    created_at       DATETIME NOT NULL DEFAULT current_timestamp, -- READONLY Folder creation timestamp, auto-generated on creation
-    updated_at       DATETIME NOT NULL DEFAULT current_timestamp, -- Folder last update timestamp, auto-updated on every update, please omit this field in the INSERT and UPDATE statements
-    created_by       INTEGER  NOT NULL,                           -- READONLY Reference to the user who owns the folder
-    updated_by       INTEGER  NOT NULL,                           -- Reference to the user who last updated the folder
-    resource_id      INTEGER  NOT NULL UNIQUE,                    -- Reference to the resource table
-    FOREIGN KEY (parent_folder_id) REFERENCES folders(id)
+    id               SERIAL PRIMARY KEY,
+    resource_id      INTEGER      NOT NULL UNIQUE,
+    name             VARCHAR(255) NOT NULL,
+    parent_folder_id INTEGER               DEFAULT NULL REFERENCES folders
+        ON DELETE CASCADE,
+    created_at       TIMESTAMP    NOT NULL DEFAULT current_timestamp,
+    updated_at       TIMESTAMP    NOT NULL DEFAULT current_timestamp,
+    created_by       INTEGER      NOT NULL,
+    updated_by       INTEGER      NOT NULL,
+    FOREIGN KEY (resource_id) REFERENCES resources(id)
         ON DELETE CASCADE,
     FOREIGN KEY (created_by) REFERENCES users(id),
-    FOREIGN KEY (updated_by) REFERENCES users(id),
-    FOREIGN KEY (resource_id) REFERENCES resources(id)
-        ON DELETE CASCADE
+    FOREIGN KEY (updated_by) REFERENCES users(id)
 );
 
-DROP INDEX IF EXISTS folders_resource_id_index;
+COMMENT ON TABLE folders IS 'Folders are a kind of specialized resources that represent the hierarchical folders structure. The parent-child relationship is defined by a self-referencing foreign key for the subfolders';
+COMMENT ON COLUMN folders.id IS 'Folder ID';
+COMMENT ON COLUMN folders.resource_id IS 'Reference to the resource table';
+COMMENT ON COLUMN folders.name IS 'Folder name; has to be unique within the parent folder';
+COMMENT ON COLUMN folders.parent_folder_id IS 'Reference to the parent folder; NULL by default for top level folders';
+COMMENT ON COLUMN folders.created_at IS 'Folder creation timestamp; auto-generated on creation';
+COMMENT ON COLUMN folders.updated_at IS 'Folder last update timestamp; auto-updated on every update';
+COMMENT ON COLUMN folders.created_by IS 'Reference to the user who owns the folder';
+COMMENT ON COLUMN folders.updated_by IS 'Reference to the user who last updated the folder';
+
+
 CREATE INDEX IF NOT EXISTS folders_resource_id_index ON folders(resource_id);
 
 
-DROP INDEX IF EXISTS folders_parent_folder_id_index;
+
 CREATE INDEX IF NOT EXISTS folders_parent_folder_id_index ON folders(parent_folder_id);
 
 
-DROP TRIGGER IF EXISTS folders_name_unique_within_parent;
-CREATE TRIGGER IF NOT EXISTS folders_name_unique_within_parent
+CREATE OR REPLACE FUNCTION validate_unique_folder_name() RETURNS TRIGGER AS
+$$
+BEGIN
+    IF exists(
+             SELECT 1 FROM folders f WHERE new.parent_folder_id = f.parent_folder_id AND f.name = new.name
+             ) THEN
+        RAISE EXCEPTION 'Folder name must be unique within the parent folder';
+    END IF;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER folders_name_unique_within_parent_trigger
     BEFORE INSERT
     ON folders
     FOR EACH ROW
-BEGIN
-    SELECT raise(ABORT, 'Folder name must be unique within the parent folder')
-     WHERE exists (
-                  SELECT 1 FROM folders f WHERE f.parent_folder_id = new.parent_folder_id AND f.name = new.name
-                  );
-END;
+EXECUTE FUNCTION validate_unique_folder_name();
 
-DROP TRIGGER IF EXISTS folders_updated_at_trigger;
-CREATE TRIGGER IF NOT EXISTS folders_updated_at_trigger
-    AFTER UPDATE
+
+
+CREATE OR REPLACE FUNCTION update_folders_timestamp() RETURNS TRIGGER AS
+$$
+BEGIN
+    new.updated_at = current_timestamp;
+    RETURN new;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER folders_updated_at_trigger
+    BEFORE UPDATE
     ON folders
     FOR EACH ROW
-BEGIN
-    UPDATE folders SET updated_at = current_timestamp WHERE id = new.id;
-END;
+EXECUTE FUNCTION update_folders_timestamp();
