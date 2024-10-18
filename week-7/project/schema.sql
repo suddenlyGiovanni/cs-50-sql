ea87c84e88f763703043f69468436c0dd8f3c42a
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-18 15:10:04 +0200

    Rename and update schema DDL file names.
    Adjusted the schema DDL file names to use three digits for better sequence sorting. Additionally, added schema control properties to the data source configuration for more automatic handling of schemas.

--- week-7/project/schema.sql
+++ week-7/project/schema.sql
@@ -1,14 +1,15 @@
 -- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
 
-\i schema_ddl/00_database.sql
-\i schema_ddl/01_users.sql
-\i schema_ddl/02_active_users_view.sql
-\i schema_ddl/03_roles.sql
-\i schema_ddl/04_permissions.sql
-\i schema_ddl/05_role_permissions.sql
-\i schema_ddl/06_role_permissions_matrix_view.sql
-\i schema_ddl/07_resources.sql
-\i schema_ddl/08_folders.sql
-\i schema_ddl/09_files.sql
-\i schema_ddl/10_user_role_resource.sql
-\i schema_ddl/11_user_role_resource_access_view.sql
+
+\i schema_ddl/000-database.sql
+\i schema_ddl/001-users.sql
+\i schema_ddl/002-active_users_view.sql
+\i schema_ddl/003-roles.sql
+\i schema_ddl/004-permissions.sql
+\i schema_ddl/005-role_permissions.sql
+\i schema_ddl/006-role_permissions_matrix_view.sql
+\i schema_ddl/007-resources.sql
+\i schema_ddl/008-folders.sql
+\i schema_ddl/009-files.sql
+\i schema_ddl/010-user_role_resource.sql
+\i schema_ddl/011-user_role_resource_access_view.sql
