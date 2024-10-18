b95042a9018249828c93508787aefd2e660604d9
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-18 13:53:42 +0200

    Simplify database cleanup script
    Combined table and function drops into streamlined commands. Removed redundant lines and ensured consistency by using CASCADE for comprehensive deletions.

--- week-7/project/vacuum.sql
+++ week-7/project/vacuum.sql
@@ -1,44 +1,21 @@
-DROP VIEW IF EXISTS active_users;
+-- Drop all tables
 
-DROP TABLE IF EXISTS files;
-DROP INDEX IF EXISTS files_parent_folder_id_index;
-DROP TRIGGER IF EXISTS files_name_unique_within_parent;
-DROP TRIGGER IF EXISTS files_updated_at_trigger;
-
-
-DROP TABLE IF EXISTS folders;
-DROP INDEX IF EXISTS folders_resource_id_index;
-DROP INDEX IF EXISTS folders_parent_folder_id_index;
-DROP TRIGGER IF EXISTS folders_updated_at_trigger;
-DROP TRIGGER IF EXISTS folders_name_unique_within_parent;
+DROP TABLE IF EXISTS files CASCADE;
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS permissions CASCADE;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS roles CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 
-DROP TABLE IF EXISTS permissions;
-DROP INDEX IF EXISTS permissions_permission_name_index;
-DROP TRIGGER IF EXISTS prevent_update_permissions;
-DROP TRIGGER IF EXISTS prevent_delete_permissions;
-DROP TRIGGER IF EXISTS prevent_insert_permissions;
+-- Drop dangling functions
+DROP FUNCTION IF EXISTS files_validate_unique_name();
+DROP FUNCTION IF EXISTS folders_validate_unique_name();
+DROP FUNCTION IF EXISTS permissions_seal();
+DROP FUNCTION IF EXISTS resource_update_timestamp();
+DROP FUNCTION IF EXISTS roles_seal();
+DROP FUNCTION IF EXISTS users_prevent_created_at_update();
+DROP FUNCTION IF EXISTS users_soft_delete();
 
-
-DROP TABLE IF EXISTS resources;
-DROP INDEX IF EXISTS resources_type_index;
-
-
-DROP TABLE IF EXISTS role_permissions;
-DROP TRIGGER IF EXISTS role_permissions_role_id_index;
-
-DROP VIEW IF EXISTS role_permissions_matrix;
-
-DROP TABLE IF EXISTS roles;
-DROP INDEX IF EXISTS roles_role_name_index;
-
-DROP TABLE IF EXISTS user_role_resource;
-
-
-DROP TABLE IF EXISTS users;
-DROP INDEX IF EXISTS users_username_index;
-DROP INDEX IF EXISTS users_deleted_index;
-DROP TRIGGER IF EXISTS users_prevent_created_at_update_trigger;
-DROP TRIGGER IF EXISTS users_soft_delete_trigger;
 
 VACUUM;
