40586a9d68fe240dff34597cd824e14c7c210c56
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-18 13:23:19 +0200

    Ensure RESOURCE_TYPE creation only if it does not exist
    Refactor to check the existence of RESOURCE_TYPE before creating it, preventing errors during repeated runs. Rearranged some DROP statements to preserve the integrity during the operation process.

--- week-7/project/ddl/07_resources.sql
+++ week-7/project/ddl/07_resources.sql
@@ -1,14 +1,20 @@
--- Defines the type of resource: folder or file
-
 BEGIN;
+
 DROP TABLE IF EXISTS resources;
 DROP TYPE IF EXISTS RESOURCE_TYPE;
-DROP INDEX IF EXISTS resources_type_index;
+DO
+$$
+    BEGIN
+        IF NOT exists(
+                     SELECT 1
+                       FROM pg_type
+                      WHERE typname = 'RESOURCE_TYPE'
+                     ) THEN CREATE TYPE RESOURCE_TYPE AS ENUM ('folder', 'file');
+        END IF;
+    END;
+$$;
 
-DROP TRIGGER IF EXISTS resources_updated_at_trigger ON resources;
-DROP TRIGGER IF EXISTS resources_unique_name_within_parent_trigger ON resources;
 
-CREATE TYPE RESOURCE_TYPE AS ENUM ('folder', 'file');
 
 CREATE TABLE IF NOT EXISTS resources (
     id         SERIAL        NOT NULL PRIMARY KEY,
@@ -26,6 +32,7 @@ COMMENT ON COLUMN resources.updated_at IS 'The resource last update timestamp; a
 COMMENT ON COLUMN resources.created_by IS 'Reference to the user who created the resource';
 COMMENT ON COLUMN resources.updated_by IS 'Reference to the user who last updated the resource';
 
+DROP INDEX IF EXISTS resources_type_index;
 CREATE INDEX IF NOT EXISTS resources_type_index ON resources(type);
 
 
@@ -37,7 +44,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
+DROP TRIGGER IF EXISTS resources_updated_at_trigger ON resources;
 CREATE TRIGGER resources_updated_at_trigger
     BEFORE UPDATE
     ON resources
