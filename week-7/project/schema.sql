c6de33974b9a27feb29a042399e4b4239945a27f
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-18 14:49:12 +0200

    Rename `ddl` directory to `schema_ddl`.
    Refactored project structure by renaming `ddl` to `schema_ddl` for clarity and consistency. Updated paths in respective SQL files and configuration files to reflect this change, ensuring seamless integration and functionality.

--- week-7/project/schema.sql
+++ week-7/project/schema.sql
@@ -1,14 +1,14 @@
 -- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
 
-\i ddl/0_database.sql
-\i ddl/01_users.sql
-\i ddl/02_active_users_view.sql
-\i ddl/03_roles.sql
-\i ddl/04_permissions.sql
-\i ddl/05_role_permissions.sql
-\i ddl/06_role_permissions_matrix_view.sql
-\i ddl/07_resources.sql
-\i ddl/08_folders.sql
-\i ddl/09_files.sql
-\i ddl/10_user_role_resource.sql
-\i ddl/11_user_role_resource_access_view.sql
+\i schema_ddl/00_database.sql
+\i schema_ddl/01_users.sql
+\i schema_ddl/02_active_users_view.sql
+\i schema_ddl/03_roles.sql
+\i schema_ddl/04_permissions.sql
+\i schema_ddl/05_role_permissions.sql
+\i schema_ddl/06_role_permissions_matrix_view.sql
+\i schema_ddl/07_resources.sql
+\i schema_ddl/08_folders.sql
+\i schema_ddl/09_files.sql
+\i schema_ddl/10_user_role_resource.sql
+\i schema_ddl/11_user_role_resource_access_view.sql
