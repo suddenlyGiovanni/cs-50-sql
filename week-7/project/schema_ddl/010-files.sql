0db91797af67abeb68b65788cdc2303990ab510c
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-23 14:19:15 +0200

    Optimize index management in files table
    Replaced the trigger-based uniqueness check with a unique index on the `parent_folder_id` and `name` columns. Added comments to indexes for better clarity and documentation. Removed redundant index creation for `parent_folder_id`.

--- week-7/project/schema_ddl/010-files.sql
+++ week-7/project/schema_ddl/010-files.sql
@@ -26,31 +26,13 @@ COMMENT ON COLUMN files.mime_type IS 'File MIME type; e.g. application/pdf, imag
 COMMENT ON COLUMN files.size IS 'File size in bytes';
 COMMENT ON COLUMN files.storage_path IS 'URL reference to the file; e.g., S3 URL';
 
-DROP INDEX IF EXISTS files_parent_folder_id_index;
-CREATE INDEX IF NOT EXISTS files_parent_folder_id_index ON files(parent_folder_id);
 
 DROP INDEX IF EXISTS files_resource_id_index;
 CREATE INDEX IF NOT EXISTS files_resource_id_index ON files(resource_id);
+COMMENT ON INDEX files_resource_id_index IS 'Index to enable fast lookups for the resource_id column';
 
-
-CREATE OR REPLACE FUNCTION files_validate_unique_name() RETURNS TRIGGER AS
-$$
-BEGIN
-    IF exists(
-             SELECT 1 FROM files WHERE new.parent_folder_id = files.parent_folder_id AND files.name = new.name
-             ) THEN
-        RAISE EXCEPTION 'Files name must be unique within the parent folder';
-    END IF;
-    RETURN new;
-END;
-$$ LANGUAGE plpgsql;
-
-
-DROP TRIGGER IF EXISTS files_name_unique_within_parent_trigger ON files;
-CREATE OR REPLACE TRIGGER files_name_unique_within_parent_trigger
-    BEFORE INSERT
-    ON files
-    FOR EACH ROW
-EXECUTE FUNCTION files_validate_unique_name();
+DROP INDEX IF EXISTS files_name_unique_within_parent_folder_index;
+CREATE UNIQUE INDEX files_name_unique_within_parent_folder_index ON files(parent_folder_id, name);
+COMMENT ON INDEX files_name_unique_within_parent_folder_index IS 'Unique index to enforce the unique files name within the parent folder; Enables fast lookups for the files name within the parent folder';
 
 COMMIT;
