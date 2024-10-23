a4e213bcd0535cd3b6bdd50f292e437efb1d4ffa
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-23 19:15:37 +0200

    Add mkdir function to create user folders
    This new function creates a folder in the database for a user with specific roles, ensuring validation checks for folder name, user ID, and parent folder ID. The function also inserts relevant records in related tables and includes a placeholder for future authorization validation.

--- week-7/project/schema_ddl/008-resources.sql
+++ week-7/project/schema_ddl/008-resources.sql
@@ -52,3 +52,97 @@ CREATE TRIGGER resources_updated_at_trigger
 EXECUTE FUNCTION resource_update_timestamp();
 
 COMMIT;
+
+
+CREATE OR REPLACE FUNCTION mkdir(
+    folder_name TEXT,
+    username TEXT,
+    role_type ROLE_TYPE,
+    parent_folder_id INTEGER DEFAULT NULL
+) RETURNS INTEGER
+    LANGUAGE plpgsql AS
+$$
+DECLARE
+    _user_id     INTEGER;
+    _resource_id INTEGER;
+    _folder_id   INTEGER;
+    _role_id     SMALLINT;
+BEGIN
+    -- validate folder_name
+    IF mkdir.folder_name IS NULL OR mkdir.folder_name = '' THEN
+        RAISE EXCEPTION 'Folder name cannot be null or empty string';
+    END IF;
+
+    -- validate parent_folder_id
+    IF mkdir.parent_folder_id IS NOT NULL THEN
+        IF NOT exists(
+                     SELECT 1
+                       FROM folders
+                      WHERE folders.id = mkdir.parent_folder_id
+                     ) THEN
+            RAISE EXCEPTION 'Parent folder with id % does not exist', parent_folder_id;
+        END IF;
+    END IF;
+
+    -- validate unique folder name
+    IF exists(
+             SELECT 1 FROM folders WHERE folders.parent_folder_id = mkdir.parent_folder_id AND name = mkdir.folder_name
+             ) THEN
+        RAISE EXCEPTION 'Folder with name "%" already exists in the parent folder', mkdir.folder_name;
+    END IF;
+
+    -- Validate and retrieve user_id
+    IF NOT exists (
+                  SELECT 1
+                    FROM users
+                   WHERE users.username = mkdir.username
+                  ) THEN
+        RAISE EXCEPTION 'User "%" does not exist', mkdir.username;
+    ELSE
+        SELECT users.id INTO _user_id FROM users WHERE users.username = mkdir.username;
+    END IF;
+
+    -- Validate and retrieve role_id
+    IF NOT exists (
+                  SELECT 1
+                    FROM roles
+                   WHERE roles.name = mkdir.role_type
+                  ) THEN
+        RAISE EXCEPTION 'Role % not found', mkdir.role_type;
+    ELSE
+        SELECT roles.id INTO _role_id FROM roles WHERE roles.name = mkdir.role_type;
+    END IF;
+
+    -- create a new resource
+       INSERT INTO resources (type, created_by, updated_by)
+       VALUES ('folder', _user_id, _user_id)
+    RETURNING resources.id INTO _resource_id;
+
+
+    -- Create a new folder
+       INSERT INTO folders (resource_id, parent_folder_id, name)
+       VALUES (_resource_id, mkdir.parent_folder_id, mkdir.folder_name)
+    RETURNING folders.id INTO _folder_id;
+
+    -- Add corresponding role-based access for the new resource
+    INSERT INTO user_role_resource (resource_id, user_id, role_id) VALUES (_resource_id, _user_id, _role_id);
+
+
+    -- TODO: needs to validate the authorisation of the user to create the folder
+    -- TODO: needs to return the folder_id?
+
+    RETURN _folder_id;
+
+END;
+$$;
+
+COMMENT ON FUNCTION mkdir IS 'Create a new folder for a user with the specified role
+
+Parameters:
+- folder_name (TEXT): The name of the new folder to be created
+- username (TEXT): The unique username of the user who will own the folder
+- role_type (ROLE_TYPE): The role type to be assigned to the folder
+- parent_folder_id (INTEGER, DEFAULT NULL): The ID of the parent folder, if any
+Returns:
+- INTEGER: The ID of the newly created folder
+';
--- week-7/project/seed.sql
+++ week-7/project/seed.sql
@@ -1,36 +1,43 @@
-INSERT INTO users (username, email,              hashed_password                           )
-VALUES            (
-                   'user_1', 'user.1@email.com', 'e09b4a4e5ad92e5a70aca91b9e382867246b33db')
-     ,            (
-                   'user_2', 'user.2@email.com', '7c3bcbab4141b30131d2736f8e0eb3ae93e0c2bf')
-     ,            (
-                   'user_3', 'user.3@email.com', '2a1093c4c4d5493b4cd0c67d9368c17f5532c5e4')
-     ,            (
-                   'user_4', 'user.4@email.com', '2de2972e50c06709e87918ed976244b6f93cd815');
+INSERT INTO users (username, email, hashed_password)
+VALUES ('user_1', 'user.1@email.com', 'e09b4a4e5ad92e5a70aca91b9e382867246b33db')
+     , ('user_2', 'user.2@email.com', '7c3bcbab4141b30131d2736f8e0eb3ae93e0c2bf')
+     , ('user_3', 'user.3@email.com', '2a1093c4c4d5493b4cd0c67d9368c17f5532c5e4')
+     , ('user_4', 'user.4@email.com', '2de2972e50c06709e87918ed976244b6f93cd815');
 
 
--- create a root folder
+/*
+ * Create a folder structure as follows for user_1:
+ *
+ * A
+ * ├── B1
+ * │   └── C
+ * │       └── D
+ * └── B2
+*/
+DO
+$$
+    DECLARE
+        folder_a_id  INT;
+        folder_b1_id INT;
+        folder_b2_id INT;
+        folder_c_id  INT;
+        folder_d_id  INT;
+        _role_type   ROLE_TYPE := 'owner';
+        _username    TEXT      := 'user_1';
+    BEGIN
+        -- Create folder "A"
+        SELECT INTO folder_a_id mkdir('A', _username, _role_type);
 
+        -- Create sub-folder "B1" under folder "A"
+        SELECT INTO folder_b1_id mkdir('B1', _username, _role_type, folder_a_id);
 
-BEGIN;
--- Get user ID for user_1
-  WITH user_1 AS (SELECT id FROM users WHERE username = 'user_1'
-      )
-     ,
--- Create a new resource and get its ID
-      new_resource AS ( INSERT INTO resources(type, created_by, updated_by) VALUES ('folder', (SELECT id FROM user_1
-          ), (SELECT id FROM user_1
-          )) RETURNING id
-      )
-     ,
--- Create a new resource and retrieve the new resource ID
-      new_folder AS (INSERT INTO folders(resource_id, parent_folder_id, name) VALUES ((SELECT id FROM new_resource
-          ), NULL, 'folder_1') RETURNING id
-      )
+        -- Create sub-folder "B2" under folder "A"
+        SELECT INTO folder_b2_id mkdir('B2', _username, _role_type, folder_a_id);
 
--- Add corresponding role-based access for the new resource
-INSERT
-  INTO user_role_resource (resource_id, user_id, role_id)
-VALUES (
-             (SELECT id FROM new_resource), (SELECT id FROM user_1), (SELECT id FROM roles WHERE roles.name = 'owner'));
-COMMIT;
+        -- Create sub-folder "C" under folder "B1"
+        SELECT INTO folder_c_id mkdir('C', _username, _role_type, folder_b1_id);
+
+        -- Create sub-folder "D" under folder "C"
+        SELECT INTO folder_d_id mkdir('D', _username, _role_type, folder_c_id);
+    END
+$$;
