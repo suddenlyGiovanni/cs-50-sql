169fd2655be13f7c83e1198ddb3a54a2332379e8
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-10-23 13:34:08 +0200

    Refactor user_role_resource_access_view for clarity
    Revised the view to explicitly list `resource_id`, `user_id`, and `role_id` columns from `user_role_resource` for better readability and maintainability. Added a comment to document the `user_id` column.

--- week-7/project/schema_ddl/012-user_role_resource_access_view.sql
+++ week-7/project/schema_ddl/012-user_role_resource_access_view.sql
@@ -3,7 +3,9 @@ BEGIN;
 DROP VIEW IF EXISTS user_role_resource_access_view CASCADE;
 
 CREATE OR REPLACE VIEW user_role_resource_access_view AS
-SELECT user_role_resource.*
+SELECT user_role_resource.resource_id
+     , user_role_resource.user_id
+     , user_role_resource.role_id
      , role_permissions_matrix_view.role
      , role_permissions_matrix_view.manage
      , role_permissions_matrix_view.read
@@ -13,6 +15,7 @@ SELECT user_role_resource.*
       JOIN role_permissions_matrix_view ON user_role_resource.role_id = role_permissions_matrix_view.role_id;
 
 COMMENT ON VIEW user_role_resource_access_view IS 'User-Role-Resource access view';
+COMMENT ON COLUMN user_role_resource_access_view.user_id IS 'User ID';
 COMMENT ON COLUMN user_role_resource_access_view.role IS 'Role name';
 COMMENT ON COLUMN user_role_resource_access_view.manage IS 'can manage the resource';
 COMMENT ON COLUMN user_role_resource_access_view.read IS 'can read the resource';
