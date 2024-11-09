91c17f15e610ce62b4c6151700fdc4e5ebd9beec
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-10 00:17:50 +0100

    Refactor validation logic for parent folder existence
    Extracted parent folder existence check into the `validate_parent_folder_exists` function to improve code reusability. Simplified `resources_validate_parent_folder_id` trigger function by delegating validation logic to the new function. Updated schema references to be fully qualified.

--- week-7/project/schema_ddl/009-resources_validate_parent_folder_id_trigger.sql
+++ week-7/project/schema_ddl/009-resources_validate_parent_folder_id_trigger.sql
@@ -1,45 +1,39 @@
-CREATE OR REPLACE FUNCTION resources_validate_parent_folder_id() RETURNS TRIGGER AS
+CREATE OR REPLACE FUNCTION validate_parent_folder_exists(_parent_folder_id INTEGER) RETURNS BOOLEAN AS
 $$
 BEGIN
-    /*
-    * A resource can be either a `folder` or a `file` type
-    *
-    * a valid `file` resource must have the parent_folder_ id:
-    * - that must not be null
-    * - must exist in the folder table
-    *
-    * a valid `folder` resource can either be a top level folder or a subfolder:
-    * - a top-level folder must have a null parent_folder_id
-    * - a subfolder must have a valid parent_folder_id that exists in the folder table
-    */
+    RETURN exists(
+                 SELECT 1
+                   FROM virtual_file_system.public.resources r
+                  WHERE _parent_folder_id = r.id AND r.type = 'folder'
+                 );
+END;
+$$ LANGUAGE plpgsql;
 
+
+CREATE OR REPLACE FUNCTION virtual_file_system.public.resources_validate_parent_folder_id() RETURNS TRIGGER AS
+$$
+BEGIN
     IF new.type = 'file' THEN
-        IF new.parent_folder_id IS NULL THEN RAISE EXCEPTION 'A file resource must have a parent folder'; END IF;
-        IF NOT exists(
-                     SELECT 1 FROM resources WHERE resources.id = new.parent_folder_id AND resources.type = 'folder'
-                     ) THEN
+        IF new.parent_folder_id IS NULL THEN
+            RAISE EXCEPTION 'A file resource must have a parent folder';
+        ELSIF NOT validate_parent_folder_exists(new.parent_folder_id) THEN
             RAISE EXCEPTION 'Parent folder with id "%" does not exist', new.parent_folder_id;
-
         END IF;
 
 
-    ELSIF new.type = 'folder' THEN
-        IF new.parent_folder_id IS NOT NULL THEN
-            IF NOT exists(
-                         SELECT 1 FROM resources WHERE resources.id = new.parent_folder_id AND resources.type = 'folder'
-                         ) THEN
-                RAISE EXCEPTION 'Parent folder with id "%" does not exist', new.parent_folder_id;
-            END IF;
+    ELSIF new.type = 'folder' AND new.parent_folder_id IS NOT NULL THEN
+        IF NOT validate_parent_folder_exists(new.parent_folder_id) THEN
+            RAISE EXCEPTION 'Parent folder with id "%" does not exist', new.parent_folder_id;
         END IF;
     END IF;
 
     RETURN new;
 END;
 $$ LANGUAGE plpgsql;
-COMMENT ON FUNCTION resources_validate_parent_folder_id IS 'Ensure that the parent folder exists for the folder being inserted or updated';
+COMMENT ON FUNCTION virtual_file_system.public.resources_validate_parent_folder_id IS 'Ensure that the parent folder exists for the folder being inserted or updated';
 
 CREATE OR REPLACE TRIGGER resources_validate_parent_folder_id_trigger
     BEFORE INSERT OR UPDATE OF parent_folder_id
-    ON resources
+    ON virtual_file_system.public.resources
     FOR EACH ROW
-EXECUTE FUNCTION resources_validate_parent_folder_id();
+EXECUTE FUNCTION virtual_file_system.public.resources_validate_parent_folder_id();
