789ea33e2c2ebc4e36141f5ca3e52a5ddc900a8a
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-12 15:18:55 +0100

    Refactor seed.sql for improved SQL formatting
    Reformatted SQL insert and select statements for better readability and consistency. Introduced a DO block to execute the chmod function with clearer variable declarations and usage.

--- week-7/project/seed.sql
+++ week-7/project/seed.sql
@@ -1,4 +1,5 @@
-INSERT INTO users (username, email, hashed_password)
+INSERT
+  INTO users (username, email, hashed_password)
 VALUES ('user_1', 'user.1@email.com', 'e09b4a4e5ad92e5a70aca91b9e382867246b33db')
      , ('user_2', 'user.2@email.com', '7c3bcbab4141b30131d2736f8e0eb3ae93e0c2bf')
      , ('user_3', 'user.3@email.com', '2a1093c4c4d5493b4cd0c67d9368c17f5532c5e4')
@@ -28,16 +29,20 @@ $$
         SELECT INTO folder_a_id mkdir('A', _username);
 
         -- Create sub-folder "B1" under folder "A"
-        SELECT INTO folder_aa_1_id mkdir('AA_1', _username, parent_folder_id => folder_a_id);
+        SELECT INTO folder_aa_1_id
+               mkdir('AA_1', _username, parent_folder_id => folder_a_id);
 
         -- Create sub-folder "AA_2" under folder "A"
-        SELECT INTO folder_aa_2_id mkdir('AA_2', _username, parent_folder_id => folder_a_id);
+        SELECT INTO folder_aa_2_id
+               mkdir('AA_2', _username, parent_folder_id => folder_a_id);
 
         -- Create sub-folder "AAA_1" under folder "AA_2"
-        SELECT INTO folder_aaa_1_id mkdir('AAA_1', _username, parent_folder_id => folder_aa_1_id);
+        SELECT INTO folder_aaa_1_id
+               mkdir('AAA_1', _username, parent_folder_id => folder_aa_1_id);
 
         -- Create sub-folder "AAAA_1" under folder "AAA_1"
-        SELECT INTO folder_aaaa_1_id mkdir('AAAA_1', _username, parent_folder_id => folder_aaa_1_id);
+        SELECT INTO folder_aaaa_1_id
+               mkdir('AAAA_1', _username, parent_folder_id => folder_aaa_1_id);
     END
 $$;
 
@@ -69,16 +74,16 @@ $$
         -- ACT: Attempt to create a circular dependency
            UPDATE resources
               SET parent_folder_id = (
-                                     SELECT folders.id
-                                       FROM folders
-                                      WHERE folders.name = 'AAAA_1'
-                                      LIMIT 1
+                  SELECT folders.id
+                    FROM folders
+                   WHERE folders.name = 'AAAA_1'
+                   LIMIT 1
                                      )
             WHERE resources.id = (
-                                 SELECT folders.resource_id
-                                   FROM folders
-                                  WHERE folders.name = 'A'
-                                  LIMIT 1
+                SELECT folders.resource_id
+                  FROM folders
+                 WHERE folders.name = 'A'
+                 LIMIT 1
                                  )
         RETURNING 1 INTO _update_row_count;
 
@@ -125,4 +130,20 @@ $$;
 
 
 
-SELECT virtual_file_system.public.chmod(1, 'user_2', 'admin');
+DO
+$$
+    DECLARE
+        _resource_aa_1_id INT := (
+                                     SELECT f.resource_id
+                                       FROM folders f
+                                      WHERE f.name = 'AA_1'
+                                 );
+        _user_2_id        INT := (
+                                     SELECT id
+                                       FROM users u
+                                      WHERE u.username = 'user_2'
+                                 );
+    BEGIN
+        PERFORM virtual_file_system.public.chmod(_resource_aa_1_id, _user_2_id, 'admin');
+    END
+$$;
