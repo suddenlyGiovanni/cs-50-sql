4a10e57f321fbe55aa7a9247b2761200069b7776
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-15 17:04:23 +0100

    Refactor SQL trigger to simplify schema references
    This change removes the schema prefix from function and table references in the 'files_validate_name_uniqueness_trigger.sql' script. It improves readability and consistency by directly using table and function names without the 'virtual_file_system.public' prefix.

--- week-7/project/schema_ddl/016-files_validate_name_uniqueness_trigger.sql
+++ week-7/project/schema_ddl/016-files_validate_name_uniqueness_trigger.sql
@@ -2,7 +2,7 @@ SET search_path TO virtual_file_system, public;
 
 BEGIN;
 
-CREATE OR REPLACE FUNCTION virtual_file_system.public.files_validate_name_uniqueness() RETURNS TRIGGER
+CREATE OR REPLACE FUNCTION files_validate_name_uniqueness() RETURNS TRIGGER
     LANGUAGE plpgsql AS
 $$
 DECLARE
@@ -10,18 +10,15 @@ DECLARE
 BEGIN
 
 
-    SELECT r.parent_folder_id
-      INTO _resource_parent_id
-      FROM virtual_file_system.public.resources AS r
-     WHERE r.id = new.resource_id;
+    SELECT r.parent_folder_id INTO _resource_parent_id FROM resources AS r WHERE r.id = new.resource_id;
 
 
     -- Check for the same name within the same parent folder
 
     IF exists(
         SELECT 1
-          FROM virtual_file_system.public.resources r_existing
-              JOIN virtual_file_system.public.files f_existing ON f_existing.resource_id = r_existing.id
+          FROM resources r_existing
+              JOIN files f_existing ON f_existing.resource_id = r_existing.id
          WHERE r_existing.parent_folder_id = _resource_parent_id
            AND r_existing.type = 'file'
            AND f_existing.name = new.name
@@ -37,6 +34,6 @@ CREATE OR REPLACE TRIGGER files_validate_name_uniqueness_trigger
     BEFORE INSERT OR UPDATE
     ON files
     FOR EACH ROW
-EXECUTE FUNCTION virtual_file_system.public.files_validate_name_uniqueness();
+EXECUTE FUNCTION files_validate_name_uniqueness();
 
 COMMIT;
