fde28e2985d26de2ae7eff59a16effa0ed0c9fc0
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-11-15 16:54:46 +0100

    Refactor and relocate permissions seal trigger function
    Moved the `permissions_seal` function and its trigger into a new SQL file: `005-permissions_seal_trigger.sql`. Adjusted the search path and removed the related definitions from `005-permissions.sql` for better modularization and maintainability.

--- week-7/project/schema_ddl/005-permissions.sql
+++ week-7/project/schema_ddl/005-permissions.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS permissions CASCADE;
 
 CREATE TABLE IF NOT EXISTS permissions (
     id          SMALLSERIAL PRIMARY KEY,
-    name        public.PERMISSION_TYPE NOT NULL UNIQUE,
-    description VARCHAR(255)           NOT NULL
+    name        PERMISSION_TYPE NOT NULL UNIQUE,
+    description VARCHAR(255)    NOT NULL
 );
 
 DROP INDEX IF EXISTS permissions_permission_name_index;
@@ -20,21 +20,5 @@ VALUES ('read', 'Can READ a resource, Folder or File')
      , ('delete', 'Can DELETE a resource, Folder or File')
      , ('manage', 'Can change the access control of a resource, File or Folder')
     ON CONFLICT (name) DO NOTHING;
-
-
-CREATE OR REPLACE FUNCTION permissions_seal() RETURNS TRIGGER AS
-$$
-BEGIN
-    RAISE EXCEPTION 'Modifications to the permissions table are not allowed.';
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-DROP TRIGGER IF EXISTS permissions_seal_trigger ON permissions;
-CREATE TRIGGER permissions_seal_trigger
-    BEFORE INSERT OR UPDATE OR DELETE
-    ON permissions
-    FOR EACH ROW
-EXECUTE FUNCTION permissions_seal();
-COMMENT ON TRIGGER permissions_seal_trigger ON permissions IS 'Seal the permissions table';
 
 COMMIT;
