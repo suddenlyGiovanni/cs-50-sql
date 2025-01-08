d09b75681a82df34c67b484416a3b8b1358dce2f
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-01-08 14:36:09 +0100

    Add validation tests for invalid parent folder cases
    This commit introduces tests to ensure file creation fails when using non-existent or incorrect parent folder IDs. Specifically, it adds checks for invalid folder IDs and parent resources of the wrong type, enhancing error handling and validation logic in the `touch` function. Additionally, teardown logic is updated to clean up test data effectively.

--- week-7/project/tests/029-touch-function.sql
+++ week-7/project/tests/029-touch-function.sql
@@ -9,28 +9,32 @@ DO
 $$
     DECLARE
         -- users:
-        _random_uuid         UUID     := gen_random_uuid();
-        _user_name           VARCHAR  := 'test_user_' || _random_uuid;
-        _user_email          VARCHAR  := _user_name || '@test.com';
-        _user_id             INT;
-        _invalid_user_id     INT      := (
-                                             SELECT floor(random() * (9999999 - 1000000 + 1) + 1000000)::INT
-                                         );
+        _random_uuid                 UUID     := gen_random_uuid();
+        _user_name                   VARCHAR  := 'test_user_' || _random_uuid;
+        _user_email                  VARCHAR  := _user_name || '@test.com';
+        _user_id                     INT;
+        _invalid_user_id             INT      := (
+                                                     SELECT floor(random() * (9999999 - 1000000 + 1) + 1000000)::INT
+                                                 );
 
         -- resources:
-        _resources_folder_id INT;
+        _resources_folder_id         INT;
+        _invalid_resources_folder_id INT      := (
+                                                     SELECT floor(random() * (9999999 - 1000000 + 1) + 1000000)::INT
+                                                 );
+        _invalid_resources_file_id   INT;
 
         -- folders
-        _folder_name         VARCHAR  := 'test_folder_' || _random_uuid;
-        _folder_id           INT;
+        _folder_name                 VARCHAR  := 'test_folder_' || _random_uuid;
+        _folder_id                   INT;
 
 
         -- roles:
-        _owner_role_id       SMALLINT := (
-                                             SELECT id
-                                               FROM roles
-                                              WHERE name = 'owner'::ROLE
-                                         );
+        _owner_role_id               SMALLINT := (
+                                                     SELECT id
+                                                       FROM roles
+                                                      WHERE name = 'owner'::ROLE
+                                                 );
 
     BEGIN
         -- Outer block to handle exceptions and ensure cleanup
@@ -82,6 +86,50 @@ $$
                 END IF;
             END;
 
+            -- Test 02: Should fail to create a file in a non-existent parent folder
+            BEGIN
+                PERFORM touch( --
+                        _user_id, --
+                        'wrong_type_parent_folder_test_file', --
+                        'text/plain', --
+                        _invalid_resources_folder_id, --
+                        '/path/to/file', --
+                        1024 --
+                        );
+                RAISE EXCEPTION 'Validation for wrong parent_folder_id failed to raise exception';
+            EXCEPTION
+                WHEN OTHERS THEN IF sqlerrm LIKE 'Parent folder with id "%" does not exist' THEN
+                    RAISE NOTICE 'Test 02 passed: "Should fail to create a File for a non-existent parent folder" - Expected exception: %',sqlerrm;
+                ELSE
+                    RAISE NOTICE 'Test 02 failed: "Should fail to create a File for a non-existent parent folder" - Unexpected exception: %', sqlerrm;
+                END IF;
+            END;
+
+            -- Test 03: Should fail to create a file in a parent resource that is not a folder
+            BEGIN
+                -- create a file resource
+                   INSERT
+                     INTO resources (type, created_by, updated_by, parent_folder_id)
+                   VALUES ('file', _user_id, _user_id, _resources_folder_id)
+                RETURNING resources.id INTO _invalid_resources_file_id;
+
+                PERFORM touch( --
+                        _user_id, --
+                        'non_existent_parent_folder_test_file', --
+                        'text/plain', --
+                        _invalid_resources_file_id, --
+                        '/path/to/file', --
+                        1024 --
+                        );
+                RAISE EXCEPTION 'Validation for wrong parent_folder_id failed to raise exception';
+            EXCEPTION
+                WHEN OTHERS THEN IF sqlerrm LIKE 'Parent folder with id "%" does not exist' THEN
+                    RAISE NOTICE 'Test 03 passed: "Should fail to create a File for a parent folder of wrong type" - Expected exception: %',sqlerrm;
+                ELSE
+                    RAISE NOTICE 'Test 03 failed: "Should fail to create a File for a parent folder of wrong type" - Unexpected exception: %', sqlerrm;
+                END IF;
+            END;
+
 
         EXCEPTION
             WHEN OTHERS THEN --
@@ -91,6 +139,7 @@ $$
 
         -- Tear down: Cleanup test data
         BEGIN
+            DELETE FROM virtual_file_system.public.resources r WHERE r.id = _invalid_resources_file_id;
             DELETE FROM virtual_file_system.public.files f WHERE f.id = _folder_id;
             DELETE FROM virtual_file_system.public.resources r WHERE r.id = _resources_folder_id;
             DELETE FROM virtual_file_system.public.users u WHERE u.id = _user_id;
